b34fbfc2464a8236e1ef15229aefbd3728c35130
Adrian A. Davin <aaredav@gmail.com>
2019-09-01 14:04:10 +1000

    Adding some testing files

--- propagate_constraints.py
+++ propagate_constraints.py
@@ -33,15 +33,25 @@ def read_calibrations(calibrations_file):
     '''
 
     :param calibrations_file: A fossil calibration file, PhyloBayes format
-    :return: A list of tuples containing node1, node2, maximim_age, minimum_age
+    :return: A list of tuples containing node1, node2, upper_age, lower_age
     '''
     calibrations = list()
     with open(calibrations_file) as f:
         f.readline()
         for l in f:
-            n1, n2, mxc, mc = l.strip().split("\t")
-            calibrations.append((n1,n2,float(mxc),float(mc)))
+            n1, n2, upper, lower = l.strip().split("\t")
+            calibrations.append((n1,n2,float(upper),float(lower)))
     return calibrations
+
+
+def propagate_upwards():
+    pass
+
+def propagate_downwards():
+    pass
+
+def propagate_horizontally():
+    pass
 
 
 def propagate_constraints(mtree, constraints, calibrations):
@@ -52,17 +62,99 @@ def propagate_constraints(mtree, constraints, calibrations):
     :param mtree: A ete3.Tree (use read_tree())
     :param constraints: A dict of constraints (use read_constraints())
     :param calibrations: A list of calibrations (use read_calibrations())
-    :return: A newick tree with propagatepropagate_constraints.py:56d constraints
+    :return: A newick tree with propagated constraints
     '''
 
-    pass
+    # We start by assigning each inner node of the species tree two new features
+
+    for n in mtree.traverse():
+        if not n.is_leaf():
+            n.add_feature("upper", 10000000000000) # Arbitrarily big number
+            n.add_feature("lower", 0)
+
+
+    # Then, for each calibration, we assign the calibration and propagate the constraint
+
+    
+
+    for calibration in calibrations:
+        n1, n2, upper, lower = calibration
+
+        # First, we propagate the upper constraint downwards the tree
+
+        mn1 = mtree&n1
+        mn2 = mtree&n2
+
+        mnode = mtree.get_common_ancestor(mn1, mn2)
+
+        if upper != -1.0 : # We check that the limit exists!
+            for n in mnode.traverse():
+                if not n.is_leaf():
+                    if n.upper > upper:
+                        n.upper = upper
+
+        # Second we propagate the lower constraint upwards the tree
+
+        if lower != -1.0:
+            for n in mnode.get_ancestors():
+                if n.lower < lower:
+                    n.lower = lower
 
 
 
 
+    # Now we have propagated all the calibrations upwards and downwards.
+    # It is time to use the transfers to propagate the constrains horizontally
 
+    # We might need to to it several times
 
+    changes = True
 
+    while(changes):
+
+        count_changes = 0
+
+        for dn, rcs in constraints.items():
+            for rc, wt in rcs.items():
+
+                dn_node = mtree&dn
+                rc_node = mtree&rc
+
+                if dn_node.lower < rc_node.lower:
+
+                    count_changes +=1
+
+                    dn_node.lower = rc_node.lower
+
+                    # We propagate upwards
+
+                    for n in dn_node.get_ancestors():
+                        n.lower < dn_node.lower
+                        n.lower = dn_node.lower
+
+                if dn_node.upper > rc_node.upper:
+
+                    count_changes += 1
+
+                    rc_node.upper = dn_node.upper
+
+                    # We propagate downwards
+
+                    for n in rc_node.traverse():
+                        if not n.is_leaf():
+                            if n.upper > rc_node.upper:
+                                n.upper = rc_node.upper
+        if count_changes == 0:
+            changes = False
+
+    #  The constraints has been fully propagated
+    # We print the tree with the intervals
+
+    for n in mtree.traverse():
+        if not n.is_leaf():
+            n.name = (str(n.upper) + "_" + str(n.lower))
+
+    return mtree.write(format=1)
 
 
 
@@ -76,10 +168,10 @@ if __name__ == "__main__":
 
     else:
 
-        _, species_tree, constraints_file, calibrations_file
+        _, species_tree, constraints_file, calibrations_file = sys.argv
 
         mtree = read_tree(species_tree)
         constraints = read_constraints(constraints_file)
         calibrations = read_calibrations(calibrations_file)
 
-        propagate_constraints(mtree, constraints, calibrations)
+        print(propagate_constraints(mtree, constraints, calibrations))
