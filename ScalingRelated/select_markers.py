367943740ebd581bfd0ecc8e237b0376d6104ae5
Adrian A. Davin <aaredav@gmail.com>
2019-12-09 16:55:59 +1000

    correcting selection

--- ScalingRelated/select_markers.py
+++ ScalingRelated/select_markers.py
@@ -1,8 +1,9 @@
+import numpy as np
 import os
 import sys
 import ete3
 
-def return_closest_marker(n):
+def return_closest_marker(n, n2marker):
     for i in range(2000):
         if n+i in n2marker and len(n2marker[n+i]) != 0:
             return(n+i)
@@ -16,7 +17,7 @@ def select_markers(refpath, gtreepath):
     for gt in genetrees:
         with open(os.path.join(gtreepath, gt)) as f:
             gtree = ete3.Tree(f.readline(),format=1)
-    sgt2leaves[gt] = len(gtree)
+            sgt2leaves[gt] = len(gtree)
     
     n2marker = dict()
     for k,v in sgt2leaves.items():
@@ -29,36 +30,36 @@ def select_markers(refpath, gtreepath):
     for gt in genetrees:
         with open(os.path.join(refpath, gt)) as f:
             gtree = ete3.Tree(f.readline(),format=1)
-    sps = set([x.name for x in gtree])    
-    gt2leaves[gt] = sps
+        sps = set([x.name for x in gtree])    
+        gt2leaves[gt] = sps
     
     marker2match = dict()
     print("\t".join(["SELECTED_MARKER_SIZE", "REF_MARKER_SIZE","SELECTED_MARKER_NAME","REF_MARKER_NAME"]))
-    for marker, leaves in gt2leaves.items():    
-        y.append(len(leaves))
+    for marker, leaves in gt2leaves.items():   
     
-        if len(leaves) in n2marker and len(n2marker[len(leaves)]) >= 1:        
-            x.append(len(leaves))
+        if len(leaves) in n2marker and len(n2marker[len(leaves)]) >= 1:      
+
             match =  np.random.choice(list(n2marker[len(leaves)]))
             marker2match[marker] = match
             n2marker[len(leaves)] = [x for x in n2marker[len(leaves)] if x != match]
             print("\t".join([str(len(leaves)), str(len(leaves)), str(match), str(marker)]))
         else:
-            cm = return_closest_marker(len(leaves))
+            cm = return_closest_marker(len(leaves),n2marker)
             if cm == None:
-                x.append(0)
+                pass
+
             else:
                 match =  np.random.choice(list(n2marker[cm]))
                 print("\t".join([str(cm), str(len(leaves)), str(match), str(marker)]))
                 marker2match[marker] = match
                 n2marker[cm] = [x for x in n2marker[cm] if x != match]
-                x.append(cm)
+
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 3:
-        print ("usage: python select_markers.py refpath, genetreepath")
+        print ("usage: python select_markers.py /Users/davin/Desktop/GitHub/Private/VirusScaling/Data/genetrees /Users/davin/Desktop/GitHub/Private/VirusScaling/ZombiRuns/Experiment2/G/Gene_trees/")
         exit(0)
 
     _, refpath, genetreepath  = sys.argv
