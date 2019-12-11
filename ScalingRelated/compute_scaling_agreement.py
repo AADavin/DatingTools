eee3fd98616b414a8eb12af5501429c199e5bd33
Adrian A. Davin <aaredav@gmail.com>
2019-12-11 11:43:34 +1000

    Correcting script

--- ScalingRelated/compute_scaling_agreement.py
+++ ScalingRelated/compute_scaling_agreement.py
@@ -7,7 +7,7 @@ def get_node2present(treefile):
 
     with open(treefile) as f:
         tree = ete3.Tree(f.readline(),format=1)
-
+    tree.name = "Root"
     node2present = dict()
     for n in tree.traverse("postorder"):
         n.add_feature("dist_to_present", 0)
@@ -18,7 +18,7 @@ def get_node2present(treefile):
     return node2present
 
 
-def compute_scaling_agreement(reftree, redtree):
+def compute_scaling_agreement(reftree, redtree, normalize):
     
     x = list()
     y = list()
@@ -31,8 +31,6 @@ def compute_scaling_agreement(reftree, redtree):
     mnode_order = list()
     
     for n in node_order:    
-        if n == "Root":
-            continue
         if n not in n2present_s or n not in n2present_ref:
             continue
         t1 = n2present_ref[n]
@@ -41,21 +39,27 @@ def compute_scaling_agreement(reftree, redtree):
         y.append(t2)    
         mnode_order.append(n)
     
-
-    # We inverse the distance (easier to read), by substractint 1-d
-
-
     x = np.array(x)
+    if normalize == 1:
+        x = x / np.max(x)
     y = np.array(y)
+    
+    # We inverse the distance (easier to read), by substractint 1-d
+    x = [1-x for x in x]
+    y = [1-y for y in y]
+
+
+    
+
     print("\t".join(["REF_VALUE","RED_VALUE","NODE"]))
     for v1, v2, v3 in zip(x,y,mnode_order):
         print("\t".join((str(v1),str(v2),v3)))
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
-        print ("usage: python compute_scaling_agreement.py reftree redtree")
+    if len(sys.argv) != 4:
+        print ("usage: python compute_scaling_agreement.py reftree redtree Normalize(1=True,0=False)")
         exit(0)
 
-    _, reftree, redtree  = sys.argv
-    compute_scaling_agreement(reftree, redtree)
+    _, reftree, redtree, n  = sys.argv
+    compute_scaling_agreement(reftree, redtree, int(n))
